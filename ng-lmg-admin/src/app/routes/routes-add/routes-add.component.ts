039891130b15b4a1f1d49517edb0a40bfc7f5d55
Michal Pawlicki <michalpawlicki@outlook.com>
2019-11-25 17:46:48 +0100

    added points table to routes adding panel

--- ng-lmg-admin/src/app/routes/routes-add/routes-add.component.ts
+++ ng-lmg-admin/src/app/routes/routes-add/routes-add.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {SelectionModel} from "@angular/cdk/collections";
+import {PointsService} from "../../_services/points/points.service";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-routes-add',
@@ -7,12 +11,27 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./routes-add.component.scss']
 })
 export class RoutesAddComponent implements OnInit {
-
+  // for the stepper
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) { }
+  // for the table
+  points: any;
+  columnsToDisplay = ['select', 'name', 'description', 'code'];
+  dataSource: any;
+
+  // for the selection
+  initialSelection = [];
+  allowMultiSelect = true;
+  selection = new SelectionModel(this.allowMultiSelect, this.initialSelection);
+
+  constructor(
+  private _formBuilder: FormBuilder,
+  private pointsService:PointsService
+  ) { }
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
@@ -21,6 +40,31 @@ export class RoutesAddComponent implements OnInit {
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
+
+    this.pointsService.getPoints()
+      .subscribe(
+        results => {
+          this.points = results;
+          console.log(results);
+          this.dataSource = new MatTableDataSource(this.points);
+          this.dataSource.paginator = this.paginator;
+        },
+        error => {console.error(error)},
+      )
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected == numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
   }
 
 }
