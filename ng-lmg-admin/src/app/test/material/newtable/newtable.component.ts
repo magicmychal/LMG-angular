bdc3c7480f76ef9add45ba8724c39d912efd28cc
Michal Pawlicki <michalpawlicki@outlook.com>
2019-11-25 15:11:51 +0100

    angular material tables

--- ng-lmg-admin/src/app/test/material/newtable/newtable.component.ts
+++ ng-lmg-admin/src/app/test/material/newtable/newtable.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
 import { NewtableDataSource, NewtableItem } from './newtable-datasource';
 import {PointsService} from "../../../_services/points/points.service";
 
@@ -13,13 +13,21 @@ import {PointsService} from "../../../_services/points/points.service";
 export class NewtableComponent implements AfterViewInit, OnInit {
   dataSource: any;
   columnsToDisplay = ['name', 'description', 'code'];
+  dataSource2: any;
 
   constructor(private pointsService:PointsService){}
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   ngOnInit() {
     this.pointsService.getPoints()
       .subscribe(
-        results => {this.dataSource = results; console.log(results)},
+        results => {
+          this.dataSource = results;
+          console.log(results);
+          this.dataSource2 = new MatTableDataSource(this.dataSource);
+          this.dataSource2.paginator = this.paginator;
+        },
         error => {console.error(error)},
       )
   }
