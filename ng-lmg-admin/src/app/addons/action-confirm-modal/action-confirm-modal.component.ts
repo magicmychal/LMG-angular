b4cd881919531791beb58a38cd7a7ee550fb1f73
Michal Pawlicki <michalpawlicki@outlook.com>
2019-12-23 17:28:34 +0100

    dialog confirming deletion

---
+++ ng-lmg-admin/src/app/addons/action-confirm-modal/action-confirm-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-action-confirm-modal',
+  templateUrl: './action-confirm-modal.component.html',
+  styleUrls: ['./action-confirm-modal.component.scss']
+})
+export class ActionConfirmModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
