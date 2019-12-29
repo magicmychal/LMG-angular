ee410927de6ba0ae167e89151c7e850021d2f3eb
Michal Pawlicki <michalpawlicki@outlook.com>
2019-12-29 15:30:35 +0100

    sorting targets by hierarchy

--- ng-lmg-admin/src/app/roads/roads-edit/roads-edit.component.ts
+++ ng-lmg-admin/src/app/roads/roads-edit/roads-edit.component.ts
@@ -7,6 +7,7 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {MatDialog} from "@angular/material/dialog";
 import {ActionConfirmModalComponent} from "../../addons/action-confirm-modal/action-confirm-modal.component";
+import {TargetsService} from "../../_services/targets/targets.service";
 
 @Component({
   selector: 'app-roads-edit',
@@ -36,14 +37,16 @@ export class RoadsEditComponent implements OnInit, AfterViewInit {
     private mapViewService: MapViewService,
     private _snackbar: MatSnackBar,
     private router: Router,
-    public dialog: MatDialog
-  ) { }
+    public dialog: MatDialog,
+    private targetsService: TargetsService
+  ) {
+  }
 
   get f() {
     return this.roadsForm.controls;
   }
 
-  get targetsFormArray(){
+  get targetsFormArray() {
     return this.f.points as FormArray;
   }
 
@@ -60,7 +63,7 @@ export class RoadsEditComponent implements OnInit, AfterViewInit {
       points: new FormArray([]),
       is_published: [false, Validators.required],
       is_deleted: [false, Validators.required]
-    })
+    });
 
     this.editInit();
   }
@@ -75,27 +78,28 @@ export class RoadsEditComponent implements OnInit, AfterViewInit {
     // fetch the point
     this.roadService.getRoadById(this.roadId)
       .subscribe(response => {
-        this.f.name.setValue(response['name']);
-        this.f.decoy.setValue(response['decoy']);
-        this.f.description.setValue(response['description']);
+          this.f.name.setValue(response['name']);
+          this.f.decoy.setValue(response['decoy']);
+          this.f.description.setValue(response['description']);
 
-        this.roadLocationName = response['location']['name'];
+          this.roadLocationName = response['location']['name'];
 
-        const position = [
-          response['location']['latitude'],
-          response['location']['longitude'],
-          response['location']['name']
-        ];
+          const position = [
+            response['location']['latitude'],
+            response['location']['longitude'],
+            response['location']['name']
+          ];
 
-        this.onResultClick(position);
+          this.onResultClick(position);
 
-        this.targetsArray = response['targets']
+          //this.targetsArray = response['targets'];
+          this.targetsArray = this.targetsService.sortHierarchy(response['targets']);
 
-        this.is_published = response['published']
+          this.is_published = response['published'];
 
 
-        this.spinner = false;
-      },
+          this.spinner = false;
+        },
         error => {
           let errorSnackBar = this._snackbar.open('Error loading road', 'Reload', {
             duration: 3500
@@ -125,17 +129,13 @@ export class RoadsEditComponent implements OnInit, AfterViewInit {
 
   moveTarget(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.targetsArray, event.previousIndex, event.currentIndex);
-    console.log(event.previousIndex, event.currentIndex)
     // append correct next_target_id
-    this.targetsArray[event.currentIndex]['next_target_id'] = this.targetsArray[event.currentIndex+1]['id']
-    console.log('id of the current target', this.targetsArray[event.currentIndex]['id'])
+    this.targetsArray[event.currentIndex]['next_target_id'] = this.targetsArray[event.currentIndex + 1]['id'];
   }
 
-  onSubmit(){
-    console.log('submitting', this.f)
-    console.log(this.targetsArray)
+  onSubmit() {
 
-    if(this.roadsForm.invalid){
+    if (this.roadsForm.invalid) {
       this._snackbar.open('Form invalid. Check the form for any mistakes', 'Dismiss', {
         duration: 3500
       });
@@ -147,21 +147,22 @@ export class RoadsEditComponent implements OnInit, AfterViewInit {
     this.spinner = true;
 
     // sort targets and add them to the form
-    console.log(this.targetsArray)
-    for (let index in this.targetsArray){
-      if(this.targetsArray[Number(index)+1] !== undefined) {
-        this.targetsArray[index]['next_target_id'] = this.targetsArray[Number(index)+1]['id']
+    console.log(this.targetsArray);
+    for (let index in this.targetsArray) {
+      if (this.targetsArray[Number(index) + 1] !== undefined) {
+        this.targetsArray[index]['next_target_id'] = this.targetsArray[Number(index) + 1]['id']
       }
       this.targetsFormArray.push(this._formBuilder.group({
         targetId: [this.targetsArray[index]['id']],
         pointId: [this.targetsArray[index]['point']['id'], Validators.required],
         sightseeing: [this.targetsArray[index]['explore_tip'], Validators.required],
         challenge: [this.targetsArray[index]['challenge_tip'], Validators.required],
-        nextTargetId: [this.targetsArray[index]['next_target_id']]
+        nextTargetId: [this.targetsArray[index]['next_target_id']],
+        hierarchy: [index]
       }))
     }
 
-    console.log('form ', this.f)
+    console.log('form ', this.f);
     this.roadService.updateRoad(this.f, this.roadId).subscribe(
       response => {
         this.spinner = false;
@@ -179,19 +180,19 @@ export class RoadsEditComponent implements OnInit, AfterViewInit {
     )
   }
 
-  removeTargetFromRoad(targetIndex){
+  removeTargetFromRoad(targetIndex) {
     let removedTarget = this.targetsArray[targetIndex];
     this.targetsArray.splice(targetIndex, 1);
     let removeSnackBar = this._snackbar.open('Point removed from the road', 'Undo', {
       duration: 3500
     });
     removeSnackBar.onAction().subscribe(null, null, () => {
-      this.targetsArray.splice(targetIndex,0, removedTarget)
+      this.targetsArray.splice(targetIndex, 0, removedTarget)
     })
 
   }
 
-  publishRoad(withhold?){
+  publishRoad(withhold?) {
     if (withhold == true) {
       this.f.is_published.setValue(false);
     } else {
@@ -200,13 +201,14 @@ export class RoadsEditComponent implements OnInit, AfterViewInit {
     this.roadService.updateRoad(this.f, this.roadId)
       .subscribe(
         response => {
-          this.is_published = response['published']
+          this.is_published = response['published'];
           let publishSnackbar;
           if (this.is_published == true) {
             publishSnackbar = this._snackbar.open('Road published', 'Withhold', {
               duration: 3500
             })
-          } if (this.is_published == false) {
+          }
+          if (this.is_published == false) {
             publishSnackbar = this._snackbar.open('Road Withhold', 'Republish', {
               duration: 3500
             })
@@ -223,17 +225,17 @@ export class RoadsEditComponent implements OnInit, AfterViewInit {
       )
   }
 
-  removeRoad(){
+  removeRoad() {
     // confirm action
-    const confirmDialog = this.dialog.open(ActionConfirmModalComponent)
+    const confirmDialog = this.dialog.open(ActionConfirmModalComponent);
     confirmDialog.afterClosed().subscribe(
       result => {
-        if(result['confirm'] == false){
+        if (result['confirm'] == false) {
           return;
         }
         this.f.is_deleted.setValue(true);
         let id = this.roadId;
-        console.log(this.f)
+        console.log(this.f);
         this.roadService.updateRoad(this.f, this.roadId).subscribe(
           result => {
             this._snackbar.open('Road deleted', 'Dismiss', {
@@ -242,12 +244,12 @@ export class RoadsEditComponent implements OnInit, AfterViewInit {
             this.router.navigate(['/roads']);
           },
           error => {
-            console.error(error)
+            console.error(error);
             this._snackbar.open('Something went wrong, on our side. Please try again', 'Dismiss', {
               duration: 3500
             });
           }
-            )
+        )
       }
     )
   }
