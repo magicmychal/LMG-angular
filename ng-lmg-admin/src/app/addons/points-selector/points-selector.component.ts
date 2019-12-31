a96ab0c5b97371e345bd44215f3acbaa059e321e
Michal Pawlicki <michalpawlicki@outlook.com>
2019-12-31 13:10:14 +0100

    minor changes

--- ng-lmg-admin/src/app/addons/points-selector/points-selector.component.ts
+++ ng-lmg-admin/src/app/addons/points-selector/points-selector.component.ts
@@ -85,40 +85,22 @@ export class PointsSelectorComponent implements OnInit {
     console.log('data source', this.dataSource._data._value);
     this.dataSource._data._value.shift();
 
-
-    // convert this.passedSelectedPoints to a structure that's this.points-alike
-    let newSelectedPointsArray = [];
-    for (let point of this.passedSelectedPoints) {
-      let query = {
-        "id": point['point']['id'],
-        "name": point['point']['name'],
-        "description": point['point']['description'],
-        "code": point['point']['code'],
-        "location": {
-          "id": point['point']['location']['id'],
-          "latitude": point['point']['location']['latitude'],
-          "longitude": point['point']['location']['longitude'],
-          "name": point['point']['location']['name']
+    let newSelection = []
+    for (let point of this.passedSelectedPoints){
+      let targetId = point['point']['id']
+      // look for that point in the array
+      for (let index in this.dataSource._data._value){
+        if(this.dataSource._data._value[index]['id'] == targetId ){
+         //this.dataSource._data._value = this.dataSource._data._value.splice(Number(index), 1)
+          newSelection = this.dataSource._data._value.splice(Number(index), 1)
         }
-      };
-      console.log(this.dataSource._data._value.indexOf(query))
-
-      /*// loop through data source array to find matches to remove
-      for (let index in this.dataSource._data._value) {
-        console.log(' compare ', this.dataSource._data._value[index], ' to ', query)
-        if (this.dataSource[index] == query) {
-          console.log('its a match!')
-        }
-      }*/
+      }
     }
+    console.log(newSelection)
 
 
-    // bledne odniesienie tutaj
-    for (let point of this.points) {
 
-      this.selection.select(point)
-      //this.selection['_selection'].add(point)
-    }
+
 
 
     this.spinner = false;
