0057ee8156f2ea692ff577504e94ea9cf926f735
Michal Pawlicki <michalpawlicki@outlook.com>
2020-01-04 21:46:37 +0100

    fixed submit button for incorrect login

--- ng-lmg-admin/src/app/login/login.component.ts
+++ ng-lmg-admin/src/app/login/login.component.ts
@@ -50,7 +50,7 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit() {
-    this.submitted = true;
+    //this.submitted = true;
 
     // stop here if form is invalid
     if (this.loginForm.invalid) {
